1c5154bea2dce21810d25ccc72eeeb54442145ac
h-n-tiwari <pmgc590@gmail.com>
2026-07-26 06:00:42 +0530

    Installed and use kubo-rpc-client

--- interface/src/context/voter.tsx
+++ interface/src/context/voter.tsx
@@ -1,53 +1,10 @@
-// import { createContext, useState, useEffect } from "react";
-// import type { ReactNode } from "react";
-// import Web3Modal from "web3modal";
-// import { ether, ContractRunners } from "ethers";
-// import { create as ipfsHttpClient } from "ipfs-http-client";
-// import axios from "axios";
-
-// // INTERNAL IMPORT
-// import { VotingAddress, VotingAddressABI } from './constants';
-
-// // IPFS Client
-// const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
-
-// // Contract Function
-// const fetchContract = (signerOrProvider: ContractRunner ) => new ethers.Contract(VotingAddress, VotingAddressABI, signerOrProvider);
-
-// // Types
-
-// interface VotingContextType {
-//   votingTitle: string;
-// }
-
-// interface VotingProviderProps {
-//   children: ReactNode;
-// }
-// export const VotingContext = React.createContext();
-
-// export const VotingProvider = ({ children }: VotingProviderProps) => {
-//     const votingTitle = "My first smart contract app";
-//     return (
-//         <VotingContext.Provider value={{ votingTitle }}>
-//             {children}
-//         </VotingContext.Provider>
-//     );
-// };
-
-// const Voter = () => {
-//   return (
-//     <div>voter</div>
-//   )
-// }
-
-// export default Voter;
-
 import { createContext, useState, useEffect } from "react";
 import type { ReactNode } from "react";
-import Web3Modal from "web3modal";
+// import Web3Modal from "web3modal";
 import { ethers } from "ethers";
-import { create as ipfsHttpClient } from "ipfs-http-client";
-import axios from "axios";
+import { create as kuboRpcClient } from "kubo-rpc-client";
+// import axios from "axios";
+import { useRouter } from "next/router";
 
 // INTERNAL IMPORT
 import { VotingAddress, VotingAddressABI } from "./constants";
@@ -55,9 +12,13 @@ import type { ContractRunner } from "ethers";
 
 // IPFS Client
 
-const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+// Using the Kubo RPC client library to talk to an IPFS node
 
-// // Contract Function
+const client = kuboRpcClient(
+    "https://ipfs.infura.io:5001/api/v0",
+);       
+
+// Contract Function
 
 const fetchContract = (signerOrProvider: ContractRunner) =>
     new ethers.Contract(VotingAddress, VotingAddressABI, signerOrProvider);
