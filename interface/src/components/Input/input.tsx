9f9b729869edd6ae3f0fd74b5d7999528fba2fb2
h-n-tiwari <pmgc590@gmail.com>
2026-09-01 13:48:17 +0530

    added input in allowed voters with tailwind css

--- interface/src/components/Input/input.tsx
+++ interface/src/components/Input/input.tsx
@@ -4,7 +4,7 @@ interface InputProps {
   inputType: string;
   title: string;
   placeholder?: string;
-  handleClick?: React.MouseEventHandler<HTMLInputElement>;
+  handleClick?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 const Input = ({ inputType, title, placeholder, handleClick }: InputProps) => {
@@ -12,12 +12,12 @@ const Input = ({ inputType, title, placeholder, handleClick }: InputProps) => {
     <div className="input">
       <p>{title}</p>
       {inputType === "text" ? (
-        <div className="w-full bg-[#730283] p-4 rounded-">
+        <div className="w-full bg-[#730283] p-0 rounded-[0.2rem]">
           <input
             type="text"
-            className="input_box_form"
+            className="w-full bg-transparent border-0 outline-none px-2 text-white placeholder:text-[#2d0036] placeholder:font-medium placeholder:capitalize"
             placeholder={placeholder}
-            onClick={handleClick}
+            onChange={handleClick}
           />
         </div>
       ) : (
--- interface/src/pages/allowed-voters.tsx
+++ interface/src/pages/allowed-voters.tsx
@@ -2,20 +2,16 @@ import React, { useState, useEffect, useCallback, useContext } from "react";
 import { useRouter } from "next/router";
 import { useDropzone } from "react-dropzone";
 import Image from "next/image";
-
 // INTERNAL import
-
 import { VotingContext } from "@/context/voter";
 import { Images } from "@assets/index";
 import Button from "@/components/Button/button";
 import Input from "@/components/Input/input";
-
 interface FormInput {
   name: string;
   address: string;
   position: string;
 }
-
 const AllowedVoters = () => {
   const [fileUrl, setFileUrl] = useState<string | null>(null);
   const [formInput, setFormInput] = useState<FormInput>({
@@ -23,10 +19,8 @@ const AllowedVoters = () => {
     address: "",
     position: "",
   });
-
   const router = useRouter();
   const { uploadToPinata } = useContext(VotingContext);
-
   // ------ VOTER IMAGE DROP
   const onDrop = useCallback(
     async (acceptedFiles: File[]) => {
@@ -36,7 +30,6 @@ const AllowedVoters = () => {
     },
     [uploadToPinata],
   );
-
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     accept: { "image/*": [] },
@@ -44,7 +37,6 @@ const AllowedVoters = () => {
   });
 
   // --- JSX ---
-
   return (
     <div className="createVoter">
       <div>
@@ -65,7 +57,6 @@ const AllowedVoters = () => {
             </p>
           </div>
         )}
-
         {!fileUrl && (
           <div className="sidInfo">
             <div className="sidInfo_box">
@@ -73,37 +64,31 @@ const AllowedVoters = () => {
               <p> Blockchain voting organization, provide ethereum ecosystem </p>
               <p className="sideInfo_para"> Contract Candidate </p>
             </div>
-
             <div className="card">
               {/*{voterArray.map((el, index) => (
                 <div key={index + 1} className="card_box">
                   <div className="image">
                     <img src="" alt="Voter Image" />
                   </div>
-
                   <div className="card_info">
                     <p>Name</p>
                     <p>Address</p>
                     <p>Position</p>
                   </div>
-
                 </div>
               ))}*/}
             </div>
           </div>
         )}
-
-      <div className="voter">
-        <div className="voter_container">
-          <h1> Create New Voter </h1>
-          <div className="voter_container_box">
+        <div className="voter">
+          <div className="voter_container">
+            <h1>Create New Voter</h1>
+            <div className="voter_container_box">
               <div className="voter_container_box_div">
                 <div {...getRootProps()}>
                   <input {...getInputProps()} />
-
                   <div className="voter_container_box_div_info">
                     <p>Upload File: PNG, JPG, GIF, WEBM, WEBP Max 20MB</p>
-
                     <div className="voter_container_box_div_image">
                       <Image
                         src={Images.CREATOR}
@@ -117,24 +102,39 @@ const AllowedVoters = () => {
                     <p>or Browse Media on your Device</p>
                   </div>
                 </div>
+              </div>
             </div>
-          </div>
-        </div>
-        </div>
+            <div className="input_container">
+              <Input
+                inputType="text"
+                title="Name"
+                placeholder="Voter Name"
+                handleClick={(e) =>
+                  setFormInput({ ...formInput, name: e.target.value })
+                }
+              />
+              <Input
+                inputType="text"
+                title="Address"
+                placeholder="Voter Address"
+                handleClick={(e) =>
+                  setFormInput({ ...formInput, address: e.target.value })
+                }
+              />
+              <Input
+                inputType="text"
+                title="Position"
+                placeholder="Voter Position"
+                handleClick={(e) =>
+                  setFormInput({ ...formInput, position: e.target.value })
+                }
+              />
+            </div>
 
-        <div className="input_container">
-          <Input
-            inputType="text"
-            title="Address"
-            placeholder="Voter Name"
-            handleClick={(e) =>
-              setFileUrl({ ...formInput, name: e.target.value})
-            }
-          />
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default AllowedVoters;
