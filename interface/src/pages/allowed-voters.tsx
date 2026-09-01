8c35dd907ba2c76e5f24c39795f0ed7652df169b
h-n-tiwari <pmgc590@gmail.com>
2026-09-01 17:00:13 +0530

    added tailwind css in allowed voters jsx part

--- interface/src/pages/allowed-voters.tsx
+++ interface/src/pages/allowed-voters.tsx
@@ -39,10 +39,12 @@ const AllowedVoters = () => {
 
   // --- JSX ---
   return (
-    <div className="createVoter">
+    // create voter
+    <div className="w-[95%] mx-auto my-8 grid grid-cols-[1fr_3fr_1fr] gap-8 max-[45em]:w-[90%] max-[45em]:grid-cols-1">
       <div>
         {fileUrl && (
-          <div className="voterInfo">
+          // voterInfo
+          <div className="absolute top-48 left-12 w-60 h-60 p-4 bg-[#231E39] rounded-t-[10px] rounded-br-[15px] rounded-bl-[10px] text-[#b3b8cd] text-start">
             <Image src={fileUrl} alt="Voter Image" />
             <p className="flex gap-1">
               <span>Name:</span>
@@ -84,15 +86,17 @@ const AllowedVoters = () => {
             </div>
           </div>
         )}
-        <div className="voter">
+        {/* voter */}
+        <div className="bg-[#231E39] rounded-[25px] shadow-[10px_10px_15px_rgba(0,0,0,0.35)] text-[#b3b8cd] p-8">
           <div className="voter_container">
             <h1>Create New Voter</h1>
-            <div className="voter_container_box">
+            {/* voter_container_box */}
+            <div className="text-center border border-dotted border-[#9a02ac] rounded-[5px]">
               <div className="voter_container_box_div">
                 <div {...getRootProps()}>
                   <input {...getInputProps()} />
                   <div className="voter_container_box_div_info">
-                    <p>Upload File: PNG, JPG, GIF, WEBM, WEBP Max 20MB</p>
+                    <p className="leading-[5]">Upload File: PNG, JPG, GIF, WEBM, WEBP Max 20MB</p>
                     <div className="voter_container_box_div_image">
                       <Image
                         src={Images.CREATOR}
@@ -102,13 +106,15 @@ const AllowedVoters = () => {
                         alt="File Upload"
                       />
                     </div>
-                    <p>Drag and Drop File</p>
-                    <p>or Browse Media on your Device</p>
+                    <p className="leading-[5]">Drag and Drop File</p>
+                    <p className="leading-[5]">or Browse Media on your Device</p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="input_container">
+
+            {/* Input Container */}
+            <div className="pt-8 w-[80%] mx-auto">
               <Input
                 inputType="text"
                 title="Name"
@@ -135,26 +141,32 @@ const AllowedVoters = () => {
               />
             </div>
 
-            <div className="Button">
+            {/* Button */}
+
+            <div className="text-end">
               <Button btnName="Authorized Voter" handleClick={() => {}} />
             </div>
           </div>
         </div>
       </div>
 
-      {/* -- */}
+      {/* CREATED VOTER */}
       <div className="createdVoter">
         <div className="createdVoter__info">
-          <Image src={Images.CREATOR} alt="user Profile" />
+          <Image
+            src={Images.CREATOR}
+            alt="user Profile"
+            width={150}
+            height={150}
+          />
           <p>Notice For User</p>
           <p>
             Organizer <span>0x939939...</span>
           </p>
           <p>
-            Only organizer of the voting contract can create voter
-            for voting elections.
+            Only organizer of the voting contract can create voter for voting
+            elections.
           </p>
-
         </div>
       </div>
     </div>
