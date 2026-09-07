12dcadb4cff3dafeda423ba94baf0f8647774e64
h-n-tiwari <pmgc590@gmail.com>
2026-09-07 20:50:13 +0530

    added image uploading location

--- interface/next.config.ts
+++ interface/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gateway.pinata.cloud",
+        pathname: "/ipfs/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
