c3a8045a142fb617319c763bb4f70d84d31d2a86
h-n-tiwari <pmgc590@gmail.com>
2026-09-13 12:31:52 +0530

    deployed script

--- interface/src/context/constants.ts
+++ interface/src/context/constants.ts
@@ -2,5 +2,5 @@
 
 import voting from "./Create.json" with { type: "json" };
 
-export const VotingAddress = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
+export const VotingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const VotingAddressABI = voting.abi;
